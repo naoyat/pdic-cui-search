d6d46c786e64d1a5d7643b1fb6cb1c9a2d11261f
naoya_t <naoya_t@users.sourceforge.jp>
2012-01-11 09:53:50 +0900

    STL関連マクロをutil_stl.hに移動

--- src/util_stl.h
+++ src/util_stl.h
@@ -3,6 +3,12 @@
 
 #include <string>
 #include <vector>
+#include <utility>
+
+#define rep(var,n)  for(int var=0;var<(n);var++)
+#define traverse(c,i)  for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
+#define all(c)  (c).begin(),(c).end()
+#define found(s,e)  ((s).find(e)!=(s).end())
 
 std::vector<std::string> split(std::string str, int delim=' ');
 std::string join(std::vector<std::string> strs, const std::string &delim="");
