b172876881df4bdc60e6ac7a0ddd00486b803026
naoya_t <naoya_t@users.sourceforge.jp>
2012-01-13 15:01:29 +0900

    gtestをbrew installで入る物に入れ替えたらver1.5にダウングレードしてしまって、そうするとEXPECT_EQ()でstd::pairの比較がちゃんとしてもらえなくてエラーを吐くようになったのでマクロを書いた

--- src/search_gtest.cc
+++ src/search_gtest.cc
@@ -9,9 +9,12 @@
 
 int cmp_count;
 
-TEST(search, concat_strings) {
-  EXPECT_TRUE(true);
-}
+// gtest 1.6だとEXPECT_EQ()で行けるんだけど1.5だとエラーが出るので
+#define EXPECT_SEARCH_RESULT_EQ(expected,result) do{\
+  EXPECT_EQ( expected.first, result.first ); \
+  EXPECT_EQ( expected.second.first, result.second.first ); \
+  EXPECT_EQ( expected.second.second, result.second.second ); \
+  } while(0)
 
 TEST(search, search) {
   //
@@ -51,100 +54,101 @@ TEST(search, search) {
   // *exact match*
   // matches once
   result = search(buf, offsets, word_count, (byte*)"aaron", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(0,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(0,0)), result );
   EXPECT_EQ( 5, cmp_count ); // 1+1 + 3 + 0+0
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"able", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(1,1)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(1,1)), result );
   EXPECT_EQ( 7, cmp_count ); // 1+1 + 2 + 1+2
 
   // matches twice
   result = search(buf, offsets, word_count, (byte*)"ant", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(2,3)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(2,3)), result );
   EXPECT_EQ( 6, cmp_count ); // 1+1 + 3 + 0+1
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"antenna", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(4,4)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(4,4)), result );
   EXPECT_EQ( 9, cmp_count ); // 1+1 + 1 + 3+3
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"apple", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(5,5)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(5,5)), result );
   EXPECT_EQ( 6, cmp_count ); // 1+1 + 3 + 0+1
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"aunt", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(6,6)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(6,6)), result );
   EXPECT_EQ( 6, cmp_count ); // 1+1 + 4 + 0+1
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"bear", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(7,7)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(7,7)), result );
   EXPECT_EQ( 8, cmp_count ); // 1+1 + 2 + 4+0
 
   // matches once
   result = search(buf, offsets, word_count, (byte*)"best", true);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(8,8)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(8,8)), result );
   EXPECT_EQ( 5, cmp_count ); // 1+1 + 3 + 0
 
 
   // no match ("apartment" can exist between "ant" and "apple")
   result = search(buf, offsets, word_count, (byte*)"apartment", true);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(4,5)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(4,5)), result );
   EXPECT_EQ( 5, cmp_count ); // 1+1 + 3
 
   // no match (before the first one)
   result = search(buf, offsets, word_count, (byte*)"aaa", true);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
   EXPECT_EQ( 1, cmp_count );
 
   // no match (after the last one)
   result = search(buf, offsets, word_count, (byte*)"cat", true);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(8,9)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(8,9)), result );
   EXPECT_EQ( 2, cmp_count );
 
   // illegal input
   result = search(buf, offsets, word_count, (byte*)"", true);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
   EXPECT_EQ( 0, cmp_count );
 
   result = search(buf, offsets, word_count, (byte*)NULL, true);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
   EXPECT_EQ( 0, cmp_count );
 
 
   // *non-exact match*
   result = search(buf, offsets, word_count, (byte*)"apple", false);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(5,5)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(5,5)), result );
 
   // matches 3 times ("ant"x2 + "antenna"x1)
   result = search(buf, offsets, word_count, (byte*)"ant", false);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(2,4)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(2,4)), result );
 
   // matches 7 times ("a*")
   result = search(buf, offsets, word_count, (byte*)"a", false);
-  EXPECT_EQ( std::make_pair(true,std::make_pair(0,6)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(true,std::make_pair(0,6)), result );
 
   // no match ("apartment" can exist between "ant" and "apple")
   result = search(buf, offsets, word_count, (byte*)"apartment", false);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(4,5)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(4,5)), result );
 
   // no match (before the first one)
   result = search(buf, offsets, word_count, (byte*)"@@@", false);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
 
   // no match (after the last one)
   result = search(buf, offsets, word_count, (byte*)"cat", false);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(8,9)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(8,9)), result );
 
   // illegal input
   result = search(buf, offsets, word_count, (byte*)"", false);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
 
   result = search(buf, offsets, word_count, (byte*)NULL, false);
-  EXPECT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
+  EXPECT_SEARCH_RESULT_EQ( std::make_pair(false,std::make_pair(-1,0)), result );
 
   free((void *)buf);
 }
+
