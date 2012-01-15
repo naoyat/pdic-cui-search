71e5bdb79a0666df27f3eec187fc167be560d76d
naoya_t <naoya_t@users.sourceforge.jp>
2012-01-15 13:54:03 +0900

    clone_cstr()のbug fix

--- src/util.cc
+++ src/util.cc
@@ -35,7 +35,7 @@ byte *clone_cstr(byte *data, int length, bool gc)
   if (!length) length = strlen((char *)data);
 
   void *newstr = clone((void *)data, length+1, gc);
-  newstr[length] = 0;
+  ((byte *)newstr)[length] = 0;
 
   return (byte *)newstr;
 }
