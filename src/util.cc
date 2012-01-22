ce29a81a9ba15dad552c1f8dbd7ded833d79e74b
naoya_t <naoya_t@users.sourceforge.jp>
2012-01-22 23:12:51 +0900

    util.cc: cpplint.pyが通るように書きなおしてみた

--- src/util.cc
+++ src/util.cc
@@ -1,30 +1,30 @@
-#include "util.h"
+// Copyright 2012 naoya_t. All Rights Reserved.
+#include "./util.h"
 
-#include <set>
 #include <stdlib.h>
 #include <string.h>
 
-#include "stlutil.h"
-#include "types.h"
+#include <set>
+
+#include "./stlutil.h"
+#include "./types.h"
 
 
 std::set<void*> clone_ptrs;
 
-void free_cloned_buffer(void *ptr)
-{
-  if (found(clone_ptrs,ptr)) {
+void free_cloned_buffer(void* ptr) {
+  if (found(clone_ptrs, ptr)) {
     free(ptr);
     clone_ptrs.erase(ptr);
   }
 }
-void free_all_cloned_buffers()
-{
-  traverse(clone_ptrs,ptr) free(*ptr);
+
+void free_all_cloned_buffers() {
+  traverse(clone_ptrs, ptr) free(*ptr);
   clone_ptrs.clear();
 }
 
-void *clone(void *data, size_t size, bool gc)
-{
+void* clone(void* data, size_t size, bool gc) {
   if (!data) return NULL;
   void *buf = malloc(size);
   memcpy(buf, data, size);
@@ -32,24 +32,22 @@ void *clone(void *data, size_t size, bool gc)
   return buf;
 }
 
-byte *clone_cstr(byte *data, int length, bool gc)
-{
-  if (!data) return (byte *)NULL;
-  if (!data[0] && gc) return (byte *)"";
-  if (!length) length = strlen((char *)data);
+byte* clone_cstr(byte* data, int length, bool gc) {
+  if (!data) return static_cast<byte*>NULL;
+  if (!data[0] && gc) return static_cast<byte*>"";
+  if (!length) length = strlen(static_cast<char*>data);
 
-  void *newstr = clone((void *)data, length+1, gc);
-  ((byte *)newstr)[length] = 0;
+  byte* newstr = static_cast<byte*>clone(static_cast<void*>data, length+1, gc);
+  newstr[length] = 0;
 
-  return (byte *)newstr;
+  return newstr;
 }
 
-int bstrcmp(byte *s1, byte *s2, int minimum_charcode)
-{
+int bstrcmp(byte *s1, byte *s2, int minimum_charcode) {
   if (!s1) return -1;
   if (!s2) return 1;
 
-  for (int i=0; ; ++i) {
+  for (int i = 0; ; ++i) {
     if (s1[i] < minimum_charcode) {
       if (s2[i] < minimum_charcode)
         return 0;
@@ -65,9 +63,8 @@ int bstrcmp(byte *s1, byte *s2, int minimum_charcode)
   return 0;
 }
 
-int bstrncmp(byte *s1, byte *s2, size_t n, int minimum_charcode)
-{
-  for (uint i=0; i<n; ++i) {
+int bstrncmp(byte *s1, byte *s2, size_t n, int minimum_charcode) {
+  for (uint i = 0; i < n; ++i) {
     if (s1[i] < minimum_charcode) {
       if (s2[i] < minimum_charcode)
         return 0;
@@ -82,9 +79,8 @@ int bstrncmp(byte *s1, byte *s2, size_t n, int minimum_charcode)
   return 0;
 }
 
-int bstrcicmp(byte *s1, byte *s2, int minimum_charcode)
-{
-  for (int i=0; ; ++i) {
+int bstrcicmp(byte *s1, byte *s2, int minimum_charcode) {
+  for (int i = 0; ; ++i) {
     int c1 = tolower(s1[i]), c2 = tolower(s2[i]);
     if (s1[i] < minimum_charcode) {
       if (s2[i] < minimum_charcode)
@@ -103,17 +99,15 @@ int bstrcicmp(byte *s1, byte *s2, int minimum_charcode)
   return 0;
 }
 
-int pbstrcmp(const void *s1, const void *s2)
-{
-  return bstrcmp(*(byte **)s1, *(byte **)s2);
-}
-int pbsrncmp(const void *s1, const void *s2, size_t n)
-{
-  return bstrncmp(*(byte **)s1, *(byte **)s2, n);
+int pbstrcmp(const void *s1, const void *s2) {
+  return bstrcmp(*static_cast<byte**>s1, *static_cast<byte**>s2);
 }
 
-byte *strhead(byte *ptr)
-{
+int pbsrncmp(const void *s1, const void *s2, size_t n) {
+  return bstrncmp(*static_cast<byte**>s1, *static_cast<byte**>s2, n);
+}
+
+byte *strhead(byte *ptr) {
   byte *p = ptr;
   while (*p) --p;
 
