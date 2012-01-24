1fd14b7358c257bf98b653a23909d45c16cc8036
naoya_t <naoya_t@users.sourceforge.jp>
2012-01-24 17:00:16 +0900

    sandbox/alt

---
+++ sandbox/alt.h
@@ -0,0 +1,17 @@
+// Copyright 2012 naoya_t.  All Rights Reserved.
+// Use of this source code is governed by a LGPL-style
+// license that can be found in the COPYING file.
+
+#ifndef SANDBOX_ALT_H_
+#define SANDBOX_ALT_H_
+
+#include <re2/re2.h>
+
+#include "util/types.h"
+
+//
+// alternative
+//
+void render_result_alt1(lookup_result fields, RE2 *re);
+
+#endif  // SANDBOX_ALT_H_
